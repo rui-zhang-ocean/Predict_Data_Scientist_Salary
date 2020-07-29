ca7d06fe1e1c4a57547bfad39d788fa2e1f2e25d
rui-zhang-ocean <rzhang.dal@gmail.com>
2020-07-29 15:20:18 -0400

    files for Heroku deployment

---
+++ setup.sh
@@ -0,0 +1,11 @@
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"rzhang.dal@gmail.com\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+“ > ~/.streamlit/config.toml
